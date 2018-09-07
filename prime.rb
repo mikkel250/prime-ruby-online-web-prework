1658777ef88b51ef4d25f25b179ae9a328b9e036
Mikkel Ridley <mikkel250@gmail.com>
2018-09-07 04:53:39 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,33 +1,21 @@
 # Add  code here!
 def prime?(n)
-  if n < 2
+  if (n < 2) || (n % 2 == 0) || (n % 3 == 0)
     return false
-  end
-  if n == 2
+  
+  if (n == 2) || (n == 3)
     return true
-  end
-  if n == 3
-    return true
-  end
-  if n % 2 == 0
-    return false
-  end
-  if n % 3 == 0
-    return false
-  
-  end
-  
-  i = 5
-  w = 2
-  
-  while i * i <= n
-    n % i == 0
-    return false
+  else
+    i = 5
+    w = 2
     
-    i += w
-    w = 6 - w
-    
+    while i * i <= n
+      n % i == 0
+      return false
+      
+      i += w
+      w = 6 - w
+    end
   end
-  
   true
 end
