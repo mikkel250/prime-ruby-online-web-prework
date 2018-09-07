176b770d08991a1368b02ad56313a790c2955703
Mikkel Ridley <mikkel250@gmail.com>
2018-09-07 04:56:29 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 # Add  code here!
 def prime?(n)
-  if (n < 2) || (n % 2 == 0) || (n % 3 == 0)
-    return false
-  elsif (n == 2) || (n == 3)
+  if (n == 2) || (n == 3)
     return true
+  elsif (n < 2) || (n % 2 == 0) || (n % 3 == 0)
+    return false
   else
     i = 5
     w = 2
